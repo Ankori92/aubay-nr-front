2f0e5c2f9e67578f4ccc557742d0f261055b9f93
Julien Bureau <ankori92@gmail.com>
2022-09-23 00:50:45 +0200

    L’intégralité des données des usages sont envoyées à Angular pour calculer les statistiques

--- src/app/models/stats.ts
+++ src/app/models/stats.ts
@@ -1,17 +1,28 @@
 export class Statistic {
     uri: string;
+    date: Date;
     duration: number; 
     queries: number;
     weight: number;
+    calls: number;
     checked: boolean;
-    elements: number;
 
-    constructor(uri: string, elements: number, duration: number, queries: number, weight: number, checked: boolean) {
+    constructor(uri: string, date: Date, calls: number, duration: number, queries: number, weight: number, checked: boolean) {
         this.uri = uri;
-        this.elements = elements;
+        this.date = date;
+        this.calls = calls;
         this.duration = duration;
         this.queries = queries;
         this.weight = weight;
         this.checked = checked;
+    }
+}
+
+export class StatisticsWrapper {
+    global: Statistic[];
+    byPeriod: Map<number, Statistic[]>;
+    constructor(global: Statistic[], byPeriod: Map<number, Statistic[]>) {
+        this.global = global;
+        this.byPeriod = byPeriod;
     }
 }
